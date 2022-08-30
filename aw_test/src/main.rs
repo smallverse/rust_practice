c49685af7c41617c2d1c2444b4d53884038f23fe
Administrator <zhaijunfeng2012@gmail.com>
2022-08-30 10:33:42 +0800

    aw_test

--- aw_test/src/main.rs
+++ aw_test/src/main.rs
@@ -1,8 +1,12 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index() -> impl Responder {
+    "Hello world! index"
 }
 
 #[post("/echo")]
@@ -14,6 +18,17 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index_app_state(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {}!",app_name) // <- response with app_name
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -21,6 +36,16 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(
+                // prefixes all resources and routes attached to it...
+                web::scope("/app")
+                    // ...so this handles requests for `GET /app/index.html`
+                    .route("/index.html", web::get().to(index)),
+            )
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web app"),
+            }))
+            .service(index_app_state)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
