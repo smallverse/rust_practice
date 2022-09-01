4c389bc0638d051f5c15197e8190bb19e0103f1b
Administrator <zhaijunfeng2012@gmail.com>
2022-09-01 19:36:54 +0800

    Atomic

--- aw_test/src/comm_base/result_code.rs
+++ aw_test/src/comm_base/result_code.rs
@@ -5,25 +5,7 @@ const CODE_FAILED: AtomicI32 = AtomicI32::from(1001);
 const CODE_VALIDATE_FAILED: AtomicI32 = AtomicI32::from(1002);
 const CODE_ERROR: AtomicI32 = AtomicI32::from(500);
 
-const MSG_SUCCESS: AtomicPtr<&str> =  AtomicPtr::from("success");
-const MSG_FAILED: AtomicI32 = AtomicI32::from(1001);
-const MSG_VALIDATE_FAILED: AtomicI32 = AtomicI32::from(1002);
-const MSG_ERROR: AtomicI32 = AtomicI32::from(500);
-#[allow(non_camel_case_types)]
-pub enum ResultCodeEnum {
-    Papery { CODE:},
-    // /** SUCCESS */
-    // SUCCESS(1000, "success"),
-    // /** FAILED */
-    // FAILED(1001, "failed"),
-    // /** VALIDATE_FAILED */
-    // VALIDATE_FAILED(1002, "validate_failed"),
-    // /** ERROR */
-    // ERROR(5000, "error"),
-}
-
-enum Option<T> {
-    Some(T),
-    None,
-}
-
+const MSG_SUCCESS: AtomicPtr<&str> = AtomicPtr::from(&mut "success");
+const MSG_FAILED: AtomicPtr<&str> = AtomicPtr::from(&mut "failed");
+const MSG_VALIDATE_FAILED: AtomicPtr<&str> = AtomicPtr::from(&mut "validate_failed");
+const MSG_ERROR: AtomicPtr<&str> = AtomicPtr::from(&mut "error");
--- aw_test/src/comm_base/result_vo.rs
+++ aw_test/src/comm_base/result_vo.rs
@@ -1,10 +1,23 @@
+use serde::Serialize;
+
 #[derive(Clone, Debug, PartialEq)]
-pub struct ResultVO<T> {
+pub struct ResultVO<T> where
+    T: ?Sized + Serialize,{
     /** 状态码，比如1000代表响应成功 */
     pub code: i32,
     /** 响应信息，用来说明响应情况 */
     pub msg: String,
     /** 响应的具体数据 */
     pub data: T,
+}
+
+impl ResultVO<T>{
+    pub fn gen_result(&self, code: i32,msg: String,data:  &T) -> ResultVO<T>{
+        ResultVO{
+            code,
+            msg,
+            data
+        }
+    }
 }
 
